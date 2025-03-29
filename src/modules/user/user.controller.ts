a47e9b4f9eb5b4ae94b07fa996c00e517351113c
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-03-29 01:01:54 -0600

    feat(users): endpoints to manage follow relationships between users were added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { SessionAuthGuard } from '../authentication/guards/session-auth.guard';
+import { Request } from 'express';
+import { User } from '@prisma/client';
 
-// v1/api/user
-@Controller('user')
+// v1/api/users
+@Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
+
+    // v1/api/users/follow/:following
+    @Post('follow/:following')
+    @UseGuards(SessionAuthGuard)
+    public followUser(
+        @Req() request: Request,
+        @Param('following') following: string,
+    ) {
+        const user = request.user as User;
+        return this.userService.followUser(user.id, following);
+    }
+
+    // v1/api/users/following
+    @Get('following')
+    @UseGuards(SessionAuthGuard)
+    public followingList(
+        @Req() request: Request,
+    ) {
+        const user = request.user as User;
+        return this.userService.followingList(user.id);
+    }
+
+    // v1/api/users/follow/:following
+    @Delete('follow/:following')
+    @UseGuards(SessionAuthGuard)
+    public unfollowUser(
+        @Req() request: Request,
+        @Param('following') following: string,
+    ) {
+        const user = request.user as User;
+        return this.userService.unfollowUser(user.id, following);
+    }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 import { CreateUserDto } from './model/dto/create-user.dto';
 import { UserWithoutPassword } from './model/types/user-without-password';
+import { isValidId } from 'src/util/regex';
 
 @Injectable()
 export class UserService {
@@ -24,16 +25,36 @@ export class UserService {
 
     /**
      * Find a user by his ID
-     * @param userId Id of the user
+     * @param userId The user id
+     * @param filterPassword If false, the password won't be removed from user data
      * @returns The user if found or `null`
      */
-    public async getUserById(userId: number, filterPassword?: boolean) {
-        const user = await this.prismaService.user.findFirstOrThrow({
+    public async getUserById(userId: number, filterPassword?: boolean): Promise<User | UserWithoutPassword | null> {
+        const user = await this.prismaService.user.findUnique({
             where: {
                 id: userId,
             },
         });
-        if (filterPassword) return this.filterPasswordFromUser(user);
+        if (!user) {
+            return user;
+        }
+        if (typeof filterPassword == 'boolean' && !filterPassword) {
+            return user;
+        }
+        return this.filterPasswordFromUser(user);
+    }
+
+    /**
+     * Find a user by his ID, throw a `NotFoundException` if not found
+     * @param userId The user id
+     * @param filterPassword If false, the password won't be removed from user data
+     * @returns The user if found, otherwise throw an exception
+     */
+    public async getUserByIdOrThrow(userId: number, filterPassword?: boolean): Promise<User | UserWithoutPassword> {
+        const user = await this.getUserById(userId, filterPassword);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
         return user;
     }
 
@@ -85,5 +106,115 @@ export class UserService {
             }
         });
         return userUpdated;
+    }
+
+    /**
+     * Creates follow relationship between 2 users
+     * @param followerId The follower id
+     * @param following The followee id as string (it will be validated)
+     * @returns The HttpResponse
+     */
+    public async followUser(followerId: number, following: string) {
+        if (!isValidId(following)) {
+            throw new BadRequestException('User ID must be a number');
+        }
+        const followingId = parseInt(following);
+        if (followerId == followingId) {
+            throw new BadRequestException('Follower and followee can\'t be the same user');
+        }
+        await this.getUserByIdOrThrow(followingId);
+        const params = {
+            followerId,
+            followingId,
+        };
+        const relation = await this.prismaService.userFollow.findUnique({
+            where: {
+                followerId_followingId: params,
+            }
+        });
+        if (relation) {
+            return { message: 'The follow relationship already exists', };
+        }
+        await this.prismaService.userFollow.create({ data: params, });
+        return { message: 'Follow relationship created', };
+    }
+
+    /**
+     * Retrieves the users list followed by a user
+     * @param followerId The follower id
+     * @returns The HttpResponse with the list of users followed
+     */
+    public async followingList(followerId: number) {
+        const following = await this.prismaService.userFollow.findMany({
+            where: { followerId }
+        });
+        const users = await Promise.all(
+            following.map(followRelation => {
+                return this.getUserById(followRelation.followingId);
+            })
+        );
+        const usersFormatted = users.map((user: User) => this.filterUserData(user));
+        return { following: usersFormatted };
+    }
+
+    /**
+     * Deletes follow relationship between 2 users
+     * @param followerId The follower id
+     * @param following The followee id as string (it will be validated)
+     * @returns The HttpResponse
+     */
+    public async unfollowUser(followerId: number, following: string) {
+        if (!isValidId(following)) {
+            throw new BadRequestException('User ID must be a number');
+        }
+        const followingId = parseInt(following);
+        const params = {
+            followerId,
+            followingId,
+        };
+        const relation = await this.prismaService.userFollow.findUnique({
+            where: {
+                followerId_followingId: params,
+            },
+        });
+        if (!relation) {
+            return { message: 'The follow relationship didn\'t exist' };
+        }
+        await this.prismaService.userFollow.delete({
+            where: {
+                followerId_followingId: params,
+            }
+        });
+        return { message: 'Follow relationship deleted' };
+    }
+
+    /**
+     * Filter user data before sending to client
+     * @param user The user data
+     * @returns New object without password, emailValidated
+     */
+    public filterUserData(user: User) {
+        const {
+            avatarImagePath,
+            bio,
+            email,
+            familyName,
+            givenName,
+            id,
+            location,
+            username,
+            website,
+        } = user;
+        return {
+            avatarImagePath,
+            bio,
+            email,
+            familyName,
+            givenName,
+            id,
+            location,
+            username,
+            website,
+        };
     }
 }
