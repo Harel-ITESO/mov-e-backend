7fc88d1222bfc34cfcbdb065747cb5ed4559a0f0
harelog3 <harelolguin@MacBook-Pro-de-Harel.local>
2025-04-03 12:35:03 -0600

    feat(account summary data handler): handle the account data updates

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,14 @@ async function bootstrap() {
 
     app.use(cookieParser(EnvConfigService.getCookieSecret())); // Parse cookies
 
-    app.useGlobalPipes(new ValidationPipe({ transform: true })); // Validate DTOs recieved on controllers
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true, // This removes properties that are not in the DTO
+            forbidNonWhitelisted: true, // Optional: This throws an error if non-whitelisted properties are provided
+        }),
+    ); // Validate DTOs received on controllers
+
     app.useGlobalFilters(new PrismaClientExceptionFilter());
     await app.listen(8080);
 }
